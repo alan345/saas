7b063a58b6cc5e9f4691c8fa1c08f77aa732a99d
alan345 <alan345@gmail.com>
2018-01-03 09:54:34 -0800

    history server pupulated

--- server/models/quote.model.js
+++ server/models/quote.model.js
@@ -12,8 +12,98 @@ var quote = new Schema({
 
     // ownerQuotes: [{type: Schema.Types.ObjectId, ref: 'User'}],
     clients: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    historyClients: [],
-    historyClientsCross: [],
+    historyClients: [{
+      // ownerCompanies: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
+      // canBeSeenByCompanies: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
+      // companies: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
+      // isAdminOfHisCompanie:{type: Boolean, default: [false]},
+      // isExternalUser:{type: Boolean, default: [false]},
+      // dateSeeLatestNotif: {type: Date, default: [Date()]},
+      email: {type: String},
+      // password: {type: String, required: true},
+      // forms: [{type: Schema.Types.ObjectId, ref: 'Form'}],
+      // paiement: {
+      //   stripe:[{
+      //     companies:[{type: Schema.Types.ObjectId, ref: 'Companie'}],
+      //     cusId:{type: String, default: ['']},
+      //     planDetail:{
+      //       plan:{type: String, default: ['']},
+      //       current_period_end: {type: Date, default: [Date()]},
+      //     }
+      //   }]
+      // },
+      // salesMan: [{type: Schema.Types.ObjectId, ref: 'User'}],
+      // resetPasswordToken: String,
+      // resetPasswordExpires: String,
+      // role: {type: Array, default: ['client']},
+      // rights: [{type: Schema.Types.ObjectId, ref: 'Right'}],
+      // rightsInApp: [],
+      // typeUsers: {type: Array},
+      profile : {
+        // profilePicture : [{type: Schema.Types.ObjectId, ref: 'Form'}],
+        language: {type: String, default: ['fr']},
+        name: {type: String, default: ['']},
+        // sourceContact: {type: String, default: ['']},
+        companyName: {type: String, default: ['']},
+        // fax:{type: String, default: ['']},
+        title: {type: String, default: ['']},
+        lastName: {type: String, default: ['']},
+        phoneNumber:{type: String, default: ['']},
+        typeClient:{type: String, default: ['']},
+        // colorCalendar:{type: String, default: ['#ad2121']},
+        // statusHouse:{type: String, default: ['']},
+        // otherData:{type: String, default: ['']},
+
+        // address:[{
+        //   nameAddress:  {type: String, default: ['']},
+        //   address : {type: String, default: ['']},
+        //   address2 : {type: String, default: ['']},
+        //   city : {type: String, default: ['']},
+        //   state : {type: String, default: ['']},
+        //   zip : {type: String, default: ['']},
+        //   country : {type: String, default: ['']},
+        // }],
+      }
+    }],
+    historyClientsCross: [{
+      // ownerCompanies: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
+
+      // companies: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
+      // isAdminOfHisCompanie:{type: Boolean, default: [false]},
+
+      // users: [{type: Schema.Types.ObjectId, ref: 'User'}],
+      // forms: [{type: Schema.Types.ObjectId, ref: 'Form'}],
+
+      // role: {type: Array, default: ['client']},
+
+      // rightsInApp: [],
+      // typeUsers: {type: Array},
+      profile : {
+        profilePicture : [{type: Schema.Types.ObjectId, ref: 'Form'}],
+        language: {type: String, default: ['fr']},
+        name: {type: String, default: ['']},
+        // sourceContact: {type: String, default: ['']},
+        companyName: {type: String, default: ['']},
+        // fax:{type: String, default: ['']},
+        title: {type: String, default: ['']},
+        lastName: {type: String, default: ['']},
+        phoneNumber:{type: String, default: ['']},
+        // typeClient:{type: String, default: ['']},
+        // colorCalendar:{type: String, default: ['#ad2121']},
+        // statusHouse:{type: String, default: ['']},
+        otherData:{type: String, default: ['']},
+
+        address:[{
+          nameAddress:  {type: String, default: ['']},
+          address : {type: String, default: ['']},
+          address2 : {type: String, default: ['']},
+          city : {type: String, default: ['']},
+          state : {type: String, default: ['']},
+          zip : {type: String, default: ['']},
+          country : {type: String, default: ['']},
+        }],
+      }
+    }],
     legalApprovals: [{type: String, default: ['']}],
     typeIntervention: {type: String, default: ['']},
     // companieClients: [{type: Schema.Types.ObjectId, ref: 'Companie'}],
