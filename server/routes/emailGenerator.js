e2193ef3a24503a78b62e613f9b551b813d00a3c
alan345 <alan345@gmail.com>
2018-01-04 14:40:07 -0800

    ok

--- server/routes/emailGenerator.js
+++ server/routes/emailGenerator.js
@@ -35,8 +35,7 @@ module.exports = {
                 <tr>
                   <td align="center" bgcolor="#ff4351" height="150">
                     <img
-                      
-                      src="http://${req.headers.host}/assets/images/Logo-mail.png"
+                      src="http://mirabelle.io/wp-content/uploads/2017/12/Logo-Mirabelle-noir-200.png"
                       alt="Email de la part de Mirabelle" style="display: block; color: #ffffff;"
                     />
                   </td>
