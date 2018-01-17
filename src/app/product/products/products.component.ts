f90cb668fb751f0b4ddcae650ccb1def7c47efdf
GuillaumeGooPlus <guillaume@gooplus.fr>
2018-01-17 20:15:52 +0100

    Update products.component.ts

--- src/app/product/products/products.component.ts
+++ src/app/product/products/products.component.ts
@@ -22,7 +22,7 @@ import {trigger, state, style, animate, transition, keyframes} from '@angular/an
   styleUrls: ['../product.component.css'],
   animations: [
     trigger('hideShowAnimator', [
-        state('true' , style({ backgroundColor: 'green' })),
+        state('true' , style({ backgroundColor: '#5bc75c' })),
         state('false', style({ backgroundColor: 'white' })),
         transition('0 => 1', animate('.5s')),
         transition('1 => 0', animate('.7s'))
