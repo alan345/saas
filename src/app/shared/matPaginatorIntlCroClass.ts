e4eb462408e1bc776ec9ff3df0dc2d9513861642
alan345 <alan345@gmail.com>
2018-01-23 18:29:26 -0800

    clean

--- src/app/shared/matPaginatorIntlCroClass.ts
+++ src/app/shared/matPaginatorIntlCroClass.ts
@@ -6,7 +6,7 @@ export class MatPaginatorIntlCro extends MatPaginatorIntl {
 
 
   getRangeLabel = (page: number, pageSize: number, length: number) => {
-    if (length == 0 || pageSize == 0) { return `0 / ${length}`; }
+    if (length === 0 || pageSize === 0) { return `0 / ${length}`; }
 
     length = Math.max(length, 0);
 
